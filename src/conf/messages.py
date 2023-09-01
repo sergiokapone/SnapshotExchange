d3ea12b08e82d71deb56b3e5d3fd5e8f9b6ac9ef
sergiokapone <sergiy.ponomarenko@gmail.com>
2023-09-01 19:23:25 +0300

    add messages

--- src/conf/messages.py
+++ src/conf/messages.py
@@ -1,10 +1,16 @@
+### DataBase messages ###
+
 WELCOME_MESSAGE = "Hello! It is a ShapShotExchange API"
 DB_CONFIG_ERROR = "Database is not configured correctly"
 DB_CONNECT_ERROR = "Error connecting to the database"
+TOO_MANY_REQUESTS = "No more than 10 requests per minute"
 
-NOT_FOUND = 'Not Found'
+### Auth messages ###
+
+PASWORD_RESET_SUCCESS = "Password reset successfully"
+NOT_FOUND = "Not Found"
 ALREADY_EXISTS = "Account already exists"
-ALREADY_EXISTS_EMAIL = "Account already exists" 
+ALREADY_EXISTS_EMAIL = "Account already exists"
 ALREADY_EXISTS_USERNAME = "Username already taken"
 DOESNT_EXISTS = "Account doesn't exists"
 SUCCESS_CREATE_USER = "User successfully created. Check your email for confirmation."
@@ -18,10 +24,9 @@ EMAIL_CONFIRMED = "Email successfully confirmed"
 EMAIL_HAS_BEEN_SEND = "Email has been send"
 CHECK_YOUR_EMAIL = "Check your email for confirmation."
 FAIL_EMAIL_VERIFICATION = "Invalid token for email verification"
-INVALID_SCOPE = 'Invalid scope for token'
-NOT_VALIDATE_CREDENTIALS = 'Could not validate credentials'
+INVALID_SCOPE = "Invalid scope for token"
+NOT_VALIDATE_CREDENTIALS = "Could not validate credentials"
 
-TOO_MANY_REQUESTS = 'No more than 10 requests per minute'
 INVALID_URL = "Invalid url"
 USER_NOT_ACTIVE = "User is banned"
 USER_ALREADY_NOT_ACTIVE = "User already is banned"
@@ -31,16 +36,15 @@ USER_CHANGE_ROLE_TO = "User role changed to"
 USER_IS_LOGOUT = "Successfully logged out!"
 TOO_MANY_HASHTAGS = "Too many hashtags! Maximum 5."
 NO_POST_ID = "No post with this ID."
-COMM_NOT_FOUND = "Comment not found or not available."
+COMMENT_NOT_FOUND = "Comment not found or not available."
 NO_RATING = "Rating not found or not available."
 OWN_POST = "It`s not possible vote for own post."
 VOTE_TWICE = "It`s not possible to vote twice."
 OPERATION_FORBIDDEN = "Operation forbidden"
 
-ALREADY_LIKE="You have already rated"
+ALREADY_LIKE = "You have already rated"
 YOUR_PHOTO = "This is your photo, you can't like it"
-FORBIDDEN="Access denied. You don't have permission for this action."
-DELETE_SUCCESSFUL="You deleted SUCCESSFUL"
+FORBIDDEN = "Access denied. You don't have permission for this action."
+DELETE_SUCCESSFUL = "You deleted SUCCESSFUL"
 
 PASWORD_RESET_SUCCESS = "Password reset successfully"
-
