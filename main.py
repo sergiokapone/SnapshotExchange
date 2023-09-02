4009c4868045d8e4d92982060ae75aa12d0e6bfe
sergiokapone <sergiy.ponomarenko@gmail.com>
2023-09-02 11:17:00 +0300

    upd readme

--- main.py
+++ main.py
@@ -22,8 +22,8 @@ from src.conf.config import init_async_redis
 
 
 app = FastAPI(
-    debug = True,
-    title = "Snapshot Exchange",
+    debug=True,
+    title="Snapshot Exchange",
 )
 
 # Настройка CORS
@@ -36,45 +36,46 @@ app.add_middleware(
 
 
 @app.on_event("startup")
-
-async def startup():   
-
+async def startup():
     redis_cache = await init_async_redis()
 
     await FastAPILimiter.init(redis_cache)
 
 
-    
-
-@app.get("/", tags=["Root"],
-# dependencies=[Depends(RateLimiter(times=2, seconds=5))]
+@app.get(
+    "/",
+    tags=["Root"],
+    # dependencies=[Depends(RateLimiter(times=2, seconds=5))]
 )
-
-async def read_root():
-
-    return {"message": "Photo Share REST API"}
+async def root():
+    return {
+        "message": "Welcome to the Snapshot Exchange REST API!",
+        "version": "Version 1.0",
+        "description": "This API provides access to Snapshot Exchange services.",
+        "documentation": "For API documentation, visit our [Documentation Page](https://example.com/documentation).",
+        "authors": [
+            "Sergiy Ponomarenko (aka sergiokapone)",
+            "Sergiy Stepanov",
+            "Ilya Vasylevskyi",
+            "Oleksandr Tarasov",
+        ],
+        "license": "This API is distributed under the MIT License.",
+        "repository": "Find the source code on [GitHub](https://github.com/sergiokapone/SnapshotExchange).",
+    }
 
 
 @app.get("/api/healthchecker", tags=["Root"])
-
 async def healthchecker(session: AsyncSession = Depends(get_db)):
-
     try:
-
         result = await session.execute(text("SELECT 1"))
 
         rows = result.fetchall()
 
-
         if not rows:
-
             raise HTTPException(
-
                 status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-
                 detail=DB_CONFIG_ERROR,
             )
-
 
         return {"message": "You successfully connected to the database!"}
 
@@ -82,22 +83,17 @@ async def healthchecker(session: AsyncSession = Depends(get_db)):
         print(e)
 
         raise HTTPException(
-
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-
             detail=DB_CONNECT_ERROR,
         )
 
 
-
-app.include_router(auth_router, prefix='/api')
-app.include_router(users_router, prefix='/api')
-app.include_router(ratings_router, prefix='/api')
-
+app.include_router(auth_router, prefix="/api")
+app.include_router(users_router, prefix="/api")
+app.include_router(ratings_router, prefix="/api")
 
 
-if __name__ == '__main__':
-    HOST='0.0.0.0'
-    PORT=8000
-    uvicorn.run(app='main:app', host=HOST, port=PORT, reload=True)
-
+if __name__ == "__main__":
+    HOST = "0.0.0.0"
+    PORT = 8000
+    uvicorn.run(app="main:app", host=HOST, port=PORT, reload=True)
