3f16a07a5bd59c5cd671d2608a24bb27fa541f26
Sergiy Ponomarenko <sergiy.ponomarenko@gmail.com>
2023-09-02 21:56:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,18 +3,18 @@
 import uvicorn
 from datetime import datetime
 
-from fastapi import FastAPI, HTTPException, Depends, Request, status
 from fastapi.templating import Jinja2Templates
+
+from fastapi import FastAPI, HTTPException, Depends, Request, status
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi_limiter import FastAPILimiter
 from fastapi.responses import HTMLResponse
 
-
 from sqlalchemy.ext.asyncio import AsyncSession
-
 from sqlalchemy import text
 from src.conf.messages import DB_CONFIG_ERROR, DB_CONNECT_ERROR, WELCOME_MESSAGE
+
 
 from src.database.connect_db import get_db
 from src.routes.auth import router as auth_router
@@ -27,11 +27,18 @@ from src.conf.info_dict import project_info
 from src.conf.config import init_async_redis
 
 current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
 app = FastAPI(
-
     debug=True,
-
     title="Snapshot Exchange",
+)
+
+# Настройка CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -46,7 +53,6 @@ async def startup():
     await FastAPILimiter.init(redis_cache)
 
 
-
 @app.get(
     "/",
     tags=["Root"],
@@ -57,23 +63,22 @@ async def root(request: Request):
     return project_info
 
 
-@app.get("/info", response_class=HTMLResponse)
+@app.get("/info", response_class=HTMLResponse, include_in_schema=False)
 async def root(request: Request):
     project_info.update({"request": request})
     return templates.TemplateResponse(
         "index.html", project_info
     )
+
+
 @app.get("/api/healthchecker", tags=["Root"])
 async def healthchecker(session: AsyncSession = Depends(get_db)):
     try:
-
         result = await session.execute(text("SELECT 1"))
         rows = result.fetchall()
         if not rows:
             raise HTTPException(
-
                 status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-
                 detail=DB_CONFIG_ERROR,
             )
 
@@ -87,21 +92,19 @@ async def healthchecker(session: AsyncSession = Depends(get_db)):
         print(e)
 
         raise HTTPException(
-
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-
             detail=DB_CONNECT_ERROR,
         )
 
 
+app.include_router(auth_router, prefix="/api")
+app.include_router(users_router, prefix="/api")
+app.include_router(ratings_router, prefix="/api")
 app.include_router(photos_router, prefix='/api')
-app.include_router(auth_router, prefix='/api')
-app.include_router(users_router, prefix='/api')
-app.include_router(ratings_router, prefix='/api')
 
-if __name__ == '__main__':
-    HOST = 'localhost'
 
+if __name__ == "__main__":
+    HOST = "0.0.0.0"
     PORT = 8000
-
-    uvicorn.run(app='main:app', host=HOST, port=PORT, reload=True)
+    uvicorn.run(app="main:app", host=HOST, port=PORT, reload=True)
+    
