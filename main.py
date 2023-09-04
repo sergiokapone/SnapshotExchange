791d5a096b7d9ce32da44bc8facd18fb123db05e
taras55001 <taras5500@gmail.com>
2023-09-04 05:24:03 +0200

    comments

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ from src.routes.auth import router as auth_router
 from src.routes.users import router as users_router
 from src.routes.ratings import router as ratings_router
 from src.routes.photos import router as photos_router
+from src.routes.comments import router as comments_router
 from src.conf.config import settings
 from src.conf.info_dict import project_info
 
@@ -98,6 +99,7 @@ app.include_router(auth_router, prefix="/api")
 app.include_router(users_router, prefix="/api")
 app.include_router(ratings_router, prefix="/api")
 app.include_router(photos_router, prefix='/api')
+app.include_router(comments_router, prefix='/api')
 
 
 if __name__ == "__main__":
